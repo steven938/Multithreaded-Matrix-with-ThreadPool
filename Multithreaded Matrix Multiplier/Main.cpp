c2a7c16b27c8a11340b32d505b20198c6bca52c0
schen938 <s_chen@hotmail.ca>
2020-06-26 16:05:37 -0700

    Add project files.

---
+++ Multithreaded Matrix Multiplier/Main.cpp
@@ -0,0 +1,45 @@
+// Multithreaded Matrix Multiplier.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <vector>
+#include <iostream>
+
+#include "ThreadPool.h"
+
+using namespace std;
+typedef vector<vector<int>> matrix;
+
+
+ThreadPool tp(10);
+
+void randGenerate(matrix& m, int i, int dimension) {
+    for (int j = 0; j < dimension; j++) {
+        m[i][j] = rand() % INT_MAX + INT_MIN;
+    }
+}
+
+matrix randomSquareMatrix(int dimension) {
+    matrix m(dimension, vector<int>(dimension, 0));
+    cout << &m << endl;
+    for (int i = 0; i < dimension; i++) {
+        tp.enqueue(randGenerate, m, i, dimension);
+    }
+
+    return m;
+}
+
+int main()
+{
+    randomSquareMatrix(10);
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
