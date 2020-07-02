1e7ddb6b735b88b0b9c4648492ee63af8010f4c6
schen938 <s_chen@hotmail.ca>
2020-07-02 09:55:55 -0700

    added multiplier functionality and fixed bug with wait_for_completion

--- Multithreaded Matrix Multiplier/Main.cpp
+++ Multithreaded Matrix Multiplier/Main.cpp
@@ -3,43 +3,86 @@
 
 #include <vector>
 #include <iostream>
-
+#include <minmax.h>
+#include <random>
+#include <chrono>
 #include "ThreadPool.h"
 
 using namespace std;
 typedef vector<vector<int>> matrix;
 
+void printMatrix(matrix& m) {
+    for (int i = 0; i < m.size(); i++)
+        for (int j = 0; j < m[0].size(); j++)
+            // Prints ' ' if j != n-1 else prints '\n'           
+            cout << m[i][j] << " \n"[j == m[0].size() -1];
+}
 
-ThreadPool tp(10);
 
-void randGenerate(matrix& m, int i, int dimension) {
+
+void randColumn(matrix& m, int i, int dimension) {
     for (int j = 0; j < dimension; j++) {
-        m[i][j] = rand() % INT_MAX + INT_MIN;
+        int rand_num = rand();
+        m[i][j] = rand_num;
+        //cout << i << " " << j << endl;
     }
 }
 
-matrix randomSquareMatrix(int dimension) {
+matrix randomSquareMatrix(int dimension, ThreadPool& tp) {
     matrix m(dimension, vector<int>(dimension, 0));
-    cout << &m << endl;
+    //auto start = chrono::high_resolution_clock::now();
     for (int i = 0; i < dimension; i++) {
-        tp.enqueue(randGenerate, m, i, dimension);
+        tp.enqueue(randColumn, ref(m), i, dimension);
     }
-
+    tp.wait_for_completion();
+    //auto end = chrono::high_resolution_clock::now();
+    //auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
+    //cout << "duration: " << duration.count() << endl;
     return m;
 }
 
-int main()
-{
-    randomSquareMatrix(10);
+void helperMultiply(matrix& A, matrix& B, matrix& res, int dimension, int i) {
+    for (int j = 0; j < dimension; j++) {
+        for (int k = 0; k < dimension; k++) {
+            res[i][j] += A[i][k] + B[j][k];
+            // locking of res matrix is not required because each thread writes to a unique locations
+            // locking of A B matrices not required because read access only
+        }
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void multiplyMatrix(matrix& A, matrix& B, matrix& res, int dimension, ThreadPool& tp) {
+    auto start = chrono::high_resolution_clock::now();
+    for (int i = 0; i < dimension; i++) {
+        //helperMultiply(A, B, res, dimension, i);
+        tp.enqueue(helperMultiply, ref(A), ref(B), ref(res), dimension, i);
+    }
+    tp.wait_for_completion();
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
+    cout << duration.count() << endl;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+int main()
+{
+    int n = max(thread::hardware_concurrency(), 4); // dynamically generate threadpool
+    int dimension = 500;
+    ThreadPool tp(n);
+    matrix m1 = randomSquareMatrix(dimension, ref(tp));
+    matrix m2 = randomSquareMatrix(dimension, ref(tp));
+    
+    matrix result(dimension, vector<int>(dimension, 0));
+
+    multiplyMatrix(m1, m2, result, dimension, tp);
+    
+    cout << result[dimension - 1][dimension - 1] << endl;
+
+    
+}
+
+// When using thread pool, there is no overhead in creating in thread
+// therefore granularity does not affect performance
+// to maximize parallelism, we want to ensure that all threads are in use
+// each matrix operation is separated by dimension (partitioned by the i th dimension)
+// to maximize concurrency, dimension should be large
--- Multithreaded Matrix Multiplier/ThreadPool.h
+++ Multithreaded Matrix Multiplier/ThreadPool.h
@@ -18,6 +18,7 @@ public:
     template<class F, class... Args> // ... is variadic template (zero or more templates form Args)
     auto enqueue(F&& f, Args&&... args)
         ->std::future<typename std::result_of<F(Args...)>::type>;
+    void wait_for_completion();
     ~ThreadPool();
 private:
     // need to keep track of threads so we can join them
@@ -28,32 +29,60 @@ private:
     // synchronization
     std::mutex queue_mutex;
     std::condition_variable condition;
-    bool stop;
+    bool stop; // stop indiciates whether threadpool should top
+
+    // signal idleness
+    int free_count=0; // counts number of free threads
+    std::mutex count_mutex;
+    std::condition_variable count_cond, queue_check_cond; // all_available indicates whether all threads are available
+    
+    int total_threads;
 };
+
 
 // the constructor just launches some amount of workers
 inline ThreadPool::ThreadPool(size_t threads)
     : stop(false)
 {
+    total_threads = threads;
+    free_count = threads;
+
     for (size_t i = 0; i < threads; ++i)
         workers.emplace_back(
+            // create a thread with function below;
             [this]
             {
-                for (;;)
+                for (;;) // infinite loop - so keeps looking for tasks to process
                 {
-                    std::function<void()> task;
-
+                    std::function<void()> task; // task that this thread is currently processing
                     {
                         std::unique_lock<std::mutex> lock(this->queue_mutex);
                         this->condition.wait(lock,
-                            [this] { return this->stop || !this->tasks.empty(); });
-                        if (this->stop && this->tasks.empty())
-                            return;
-                        task = std::move(this->tasks.front());
-                        this->tasks.pop();
-                    }
+                            [this] { return this->stop || !this->tasks.empty(); }); //unlock lock and wait to be notified when a task gets enqueued
+                                                                                    //relock and continue when notified and stop = true or tasks is not empty
 
-                    task();
+                        if (this->stop && this->tasks.empty()) // stop thread if stop = true and task queue is empty
+                            return;
+
+                        // decrement free count
+                        {
+                            std::unique_lock<std::mutex> lock(this->count_mutex);
+                            this->free_count--;
+                        }
+
+                        task = std::move(this->tasks.front()); // takes on task at front of task queue
+                        this->tasks.pop(); // pop the task from task queue
+                    }
+                    this->queue_check_cond.notify_one(); // notify to check if queue is empty
+
+                    task(); // run the task
+                    
+                    // increment free count
+                    {
+                        std::unique_lock<std::mutex> lock(this->count_mutex);
+                        this->free_count++;
+                    }
+                    this->count_cond.notify_one(); // notify that this thread has been freed
                 }
             }
             );
@@ -62,15 +91,15 @@ inline ThreadPool::ThreadPool(size_t threads)
 // add new work item to the pool
 template<class F, class... Args>
 auto ThreadPool::enqueue(F&& f, Args&&... args)
--> std::future<typename std::result_of<F(Args...)>::type>
+-> std::future<typename std::result_of<F(Args...)>::type> // return type of enqueue function
 {
     using return_type = typename std::result_of<F(Args...)>::type;
 
-    auto task = std::make_shared< std::packaged_task<return_type()> >(
-        std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-        );
+    auto task = std::make_shared< std::packaged_task<return_type()> >( // constructs pacakged task function (an asynchronous function) and returns shared pointer
+        std::bind(std::forward<F>(f), std::forward<Args>(args)...) 
+        ); 
 
-    std::future<return_type> res = task->get_future();
+    std::future<return_type> res = task->get_future(); 
     {
         std::unique_lock<std::mutex> lock(queue_mutex);
 
@@ -80,8 +109,33 @@ auto ThreadPool::enqueue(F&& f, Args&&... args)
 
         tasks.emplace([task]() { (*task)(); });
     }
-    condition.notify_one();
+    condition.notify_one(); // unblock one task thread so that it can take on task
     return res;
+}
+
+// used for synchronization so that threadpool user knows when all tasks are completed and threads are all free
+void ThreadPool::wait_for_completion() {
+    // wait till task queue is empty
+    // queue_check_cond notifies this thread when items are popped from queue 
+    {
+        std::cout << "waiting for task queue to become empty" << std::endl;
+        std::unique_lock<std::mutex> lock(this->queue_mutex);
+        this->queue_check_cond.wait(lock, [this] {return this->tasks.empty(); }); // wait until queue is empty
+        std::cout << "task queue is now empty" << std::endl;
+    }
+    
+    // wait till all threads are free
+    {
+        std::cout << "waiting for all threads to finish processing" << std::endl;
+        std::unique_lock<std::mutex> lock1(this->count_mutex);
+        if (this->free_count == this->total_threads) {
+            std::cout << "all threads finished processing" << std::endl;
+            return;
+        }
+        this->count_cond.wait(lock1, [this] {return this->free_count == this->total_threads; });
+    }
+    std::cout << "all threads finished processing" << std::endl;
+    
 }
 
 // the destructor joins all threads
